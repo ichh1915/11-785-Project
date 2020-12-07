bc3a1ce4ea36b80dd7390c1012e2a25162a24a51
raindraconis <yuesihan@gmail.com>
2020-12-08 00:33:57 +0800

    add srresnet

--- source/neuralnet.py
+++ source/neuralnet.py
@@ -13,6 +13,8 @@ class NeuralNet(object):
           self.model = SRNET(self.ngpu).to(self.device)
         elif model == 'FSRCNN':
           self.model = FSRCNN(self.ngpu).to(self.device)
+        elif model == 'SRResNet':
+          self.model = SRResNet(self.ngpu).to(self.device)
 
         if (self.device.type == 'cuda') and (self.model.ngpu > 0):
             self.model = nn.DataParallel(self.model, list(range(self.model.ngpu)))
@@ -26,8 +28,8 @@ class NeuralNet(object):
         self.mse = nn.MSELoss()
         self.optimizer = optim.SGD(self.model.parameters(), lr=1e-3)
 
-class SRNET(nn.Module):
 
+class SRNET(nn.Module):
     def __init__(self, ngpu):
         super(SRNET, self).__init__()
 
@@ -85,3 +87,76 @@ class FSRCNN(torch.nn.Module):
         out = self.deconvolution(out)
 
         return torch.clamp(out, min=1e-12, max=1-(1e-12))
+
+
+class SRResNet(nn.Module):
+    def __init__(self, ngpu, in_channels=3, out_channels=3, B=16):
+        super(SRResNet, self).__init__()
+        self.ngpu = ngpu
+        
+        self.input = nn.Sequential(
+                        nn.Conv2d(in_channels = in_channels, out_channels=64, kernel_size=9, stride=1, padding=4, bias=False),
+                        nn.PReLU(num_parameters=1,init=0.2)
+        )
+        
+        residual = []
+        for _ in range(B):
+            residual.append(ResidualBlock())
+        self.residual = nn.Sequential(*residual)
+
+        self.ConvBN = nn.Sequential(
+                nn.Conv2d(in_channels=64, out_channels=64, kernel_size=3, stride=1, padding=1, bias=False),
+                nn.BatchNorm2d(64)
+        )
+
+        self.shuffle = nn.Sequential(
+                        ShuffleBlock(),
+                        ShuffleBlock()
+        )
+
+        self.conv = nn.Conv2d(in_channels=64, out_channels=out_channels, kernel_size=9, stride=1, padding=4, bias=False)
+
+    def forward(self, x):
+        output0 = self.input(x)
+        output = self.residual(output0)
+        output = self.ConvBN(output)
+        output = torch.add(output, output0)
+        output = self.shuffle(output)
+        output = self.conv(output)
+        
+        return output
+
+
+
+class ResidualBlock(nn.Module):
+    def __init__(self):
+        super(ResidualBlock, self).__init__()
+        
+        self.model = nn.Sequential(
+                        nn.Conv2d(in_channels=64, out_channels=64, kernel_size=3, stride=1, padding=1, bias=False),
+                        nn.BatchNorm2d(64),
+                        nn.PReLU(num_parameters=1,init=0.2),
+                        nn.Conv2d(in_channels=64, out_channels=64, kernel_size=3, stride=1, padding=1, bias=False),
+                        nn.BatchNorm2d(64)
+        )
+        
+    def forward(self, x):
+        output = self.model(x)
+        output = torch.add(output,x) # elementwise sum
+
+        return output 
+
+
+class ShuffleBlock(nn.Module):
+    def __init__(self):
+        super(ShuffleBlock, self).__init__()
+        
+        self.model = nn.Sequential(
+                        nn.Conv2d(in_channels=64, out_channels=64, kernel_size=3, stride=1, padding=1, bias=False),
+                        nn.PixelShuffle(1),
+                        nn.PReLU(num_parameters=1,init=0.2)
+        )
+        
+    def forward(self, x):
+        output = self.model(x)
+        return output 
